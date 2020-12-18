3eeac6723d6d87937e63eda2c63f428d97b7e11c
Lev <osipov.enterprise@gmail.com>
2020-12-19 00:01:07 +0300

    Corrected file://

--- lab04a/filter/filter.scala
+++ lab04a/filter/filter.scala
@@ -40,7 +40,7 @@ object filter {
 
     val kafkaServer = "spark-master-1:6667"
     val kafkaOffset = if (offset == "earliest") offset else " { \"%s\": { \"0\": %s } } ".format(kafkaTopic, offset)
-    val kafkaOutputDir = outputDir.replaceAll("^file:/", "")
+    val kafkaOutputDir = outputDir.replaceAll("^file:/", "file://")
     val kafkaParams = Map(
       "kafka.bootstrap.servers" -> kafkaServer,
       "subscribe" -> kafkaTopic,
